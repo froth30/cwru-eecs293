a3f64b21f9c22b143fde20bc3ba94e6a0ff09cc5
teddybear02 <theo.frohlich@gmail.com>
2016-09-06 23:58:47 -0400

    Suppress access warning

--- java/src/hw1/Main.java
+++ java/src/hw1/Main.java
@@ -53,6 +53,7 @@ public class Main {
      * @param <T> the type of the list elements to be compared
      * @return the prefix of maximum length that is common to both lists
      */
+    @SuppressWarnings("WeakerAccess")
     static <T> List<T> longestPrefix(List<T> a,
                                      List<T> b,
                                      Comparator<? super T> cmp)
