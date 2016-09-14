e0d035a6ddac9f516fe4f7930aa3705b7c8fec36
teddybear02 <theo.frohlich@gmail.com>
2016-09-14 05:41:29 -0400

    First revision

--- scala/src/edu/cwru/eecs293/ttf10/uxb/Device.scala
+++ scala/src/edu/cwru/eecs293/ttf10/uxb/Device.scala
@@ -1,9 +1,5 @@
 package edu.cwru.eecs293.ttf10.uxb
 
-import java.math.BigInteger
-import java.util.List
-import java.util.Optional
-
 /**
   * Represents a UXB device, which is a UXB-enabled computer, peripheral, or hub.
   *
@@ -12,58 +8,53 @@ import java.util.Optional
   * Programming Assignment 2  |  Due at beginning of discussion on Wednesday, September 7, 2016
   *
   * @see <a href="Hw2.pdf">https://blackboard.case.edu/bbcswebdav/pid-1379538-dt-content-rid-4276971_1/courses/eecs293_vxl11/Hw2.pdf</a>
+  *
   * @author Theodore Frohlich <ttf10@case.edu>
   */
 trait Device {
   /**
     * Returns the product code of this device.
-    *
     * @return the product code of this device. If the product code is unknown, return an empty optional.
     */
-  def getProductCode: Optional[Integer]
+  def getProductCode: Option[Integer]
 
   /**
     * Returns the serial number of this device.
-    *
     * @return the serial number of this device. If the serial number is unknown, return an empty optional.
     */
-  def getSerialNumber: Optional[BigInteger]
+  def getSerialNumber: Option[BigInt]
 
   /**
     * Returns the UXB version that this device supports.
-    *
     * @return the UXB version that this device supports
     */
   def getVersion: Integer
 
   /**
     * Returns the class of this UXB device.
-    *
     * @return the class of this UXB device
     */
   def getDeviceClass: DeviceClass
 
   /**
     * Returns the number of connectors that this device has.
-    *
     * @return the number of connectors that this device has
     */
   def getConnectorCount: Integer
 
   /**
     * Returns the type of each connector in this device.
-    *
     * @return the type of each connector in this device
     */
-  def getConnectors: util.List[Connector.Type]
+  def getConnectors: List[Connector.Type.Type]
 
   /**
     * Returns the connector of this device at the given index.
-    *
     * @param index the plug number of the connector in this device
     * @return the connector of this device at the given index
     */
   def getConnector(index: Int): Connector
 
   // TODO In future assignments, additional methods will be defined on the Device interface.
+
 }
