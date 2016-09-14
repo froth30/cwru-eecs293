02ccf7d3f4d6b3562ba2ee15502ad31d4a111b7b
teddybear02 <theo.frohlich@gmail.com>
2016-09-14 05:57:44 -0400

    First revision

--- scala/src/edu/cwru/eecs293/ttf10/uxb/Hub.scala
+++ scala/src/edu/cwru/eecs293/ttf10/uxb/Hub.scala
@@ -1,5 +1,7 @@
 package edu.cwru.eecs293.ttf10.uxb
 
+import edu.cwru.eecs293.ttf10.uxb.DeviceClass.DeviceClass
+
 /**
   * Represents a UXB hub, which can have multiple computer-side and peripheral-side connectors.
   *
@@ -16,25 +18,22 @@ object Hub {
 
   /**
     * Creates a new builder with the given UXB version, no connectors, and with empty product code and serial number.
-    *
     * @param version the UXB version that this device supports
     */
     extends AbstractDevice.Builder[Hub.Builder](version) {
     /**
       * Initializes the hub with the builder’s version, product code, serial number, and connector list.
-      *
       * @return the initialized hub
       * @throws IllegalStateException if the version is null, or the hub has no computer connector, or the hub has no peripheral connector.
       */
     @throws[IllegalStateException]
     def build: Hub = {
-      validate()
+      validate
       new Hub(this)
     }
 
     /**
       * Returns this builder.
-      *
       * @return this builder
       */
     protected def getThis: Hub.Builder = this
@@ -44,20 +43,21 @@ object Hub {
       * @throws IllegalStateException if and only if the version number is null, or if the hub has no computer connector, or if the hub has no peripheral connector
       */
     @throws[IllegalStateException]
-    override protected def validate() {
+    override protected def validate {
       // Check if the version number is null
-      try
-        super.validate()
-
-      catch {
-        case e: NullPointerException => {
-          throw new IllegalStateException(e)
-        }
+      try {
+        super.validate
+      } catch {
+        case e: NullPointerException => throw new IllegalStateException(e)
       }
       // Check if the hub has no computer connector
-      if (!getConnectors.contains(Connector.Type.COMPUTER)) throw new IllegalStateException("Validation failed: hub has no computer connector.")
+      if (!getConnectors.contains(Connector.Type.COMPUTER)) {
+        throw new IllegalStateException("Validation failed: hub has no computer connector.")
+      }
       // Check if the hub has no peripheral connector
-      if (!getConnectors.contains(Connector.Type.PERIPHERAL)) throw new IllegalStateException("Validation failed: hub has no peripheral connector.")
+      if (!getConnectors.contains(Connector.Type.PERIPHERAL)) {
+        throw new IllegalStateException("Validation failed: hub has no peripheral connector.")
+      }
     }
   }
 
@@ -67,7 +67,6 @@ class Hub private(val builder: Hub.Builder)
 
 /**
   * Initializes the hub from the given builder.
-  *
   * @param builder a builder for initializing the hub
   */
   extends AbstractDevice[Hub.Builder](builder) {
