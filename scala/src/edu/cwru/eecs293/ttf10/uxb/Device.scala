4f1ffd3cb49e5a4cca590aebada2f94de893dc19
teddybear02 <theo.frohlich@gmail.com>
2016-09-15 20:38:35 -0400

    Final revisions

--- scala/src/edu/cwru/eecs293/ttf10/uxb/Device.scala
+++ scala/src/edu/cwru/eecs293/ttf10/uxb/Device.scala
@@ -1,7 +1,5 @@
 package edu.cwru.eecs293.ttf10.uxb
 
-import Connector.Connector
-
 /**
   * Represents a UXB device, which is a UXB-enabled computer, peripheral, or hub.
   *
@@ -10,9 +8,12 @@ import Connector.Connector
   * Programming Assignment 2  |  Due at beginning of discussion on Wednesday, September 7, 2016
   *
   * @see <a href="Hw2.pdf">https://blackboard.case.edu/bbcswebdav/pid-1379538-dt-content-rid-4276971_1/courses/eecs293_vxl11/Hw2.pdf</a>
+  *
   * @author Theodore Frohlich <ttf10@case.edu>
   */
 trait Device {
+
+  type Connector = Connector.type
 
   /**
     * Returns the product code of this device.
@@ -48,7 +49,7 @@ trait Device {
     * Returns the type of each connector in this device.
     * @return the type of each connector in this device
     */
-  def getConnectors: List[Connector.Type]  // TODO How can this be done with less involvement?
+  def getConnectors: List[Connector.Type]
 
   /**
     * Returns the connector of this device at the given index.
