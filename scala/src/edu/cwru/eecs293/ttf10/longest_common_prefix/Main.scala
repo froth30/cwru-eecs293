4de70476e26404d3e5d082e4d3f1dbaaf22e2d8d
teddybear02 <theo.frohlich@gmail.com>
2016-09-17 01:45:41 -0400

    Fixed @see link in header doc

--- scala/src/edu/cwru/eecs293/ttf10/longest_common_prefix/Main.scala
+++ scala/src/edu/cwru/eecs293/ttf10/longest_common_prefix/Main.scala
@@ -9,7 +9,7 @@ import scala.io.StdIn
   * EECS 293  |  Software Craftsmanship  |  2016 Fall Semester
   * Due at beginning of discussion on Wednesday, September 7, 2016
   *
-  * @see <a href="Hw1.pdf">https://blackboard.case.edu/bbcswebdav/pid-1366059-dt-content-rid-4244627_1/courses/eecs293_vxl11/Hw1.pdf</a>
+  * @see [[https://blackboard.case.edu/bbcswebdav/pid-1366059-dt-content-rid-4244627_1/courses/eecs293_vxl11/Hw1.pdf Hw1.pdf]]
   *
   * @author Theodore Frohlich <ttf10@case.edu>
   */
