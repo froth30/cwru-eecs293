aaa8a902dbe1c484d1f3aad24a03e9172a000604
teddybear02 <theo.frohlich@gmail.com>
2016-09-21 00:09:40 -0400

    Repaired link to Hub in header doc

--- scala/test/edu/cwru/eecs293/ttf10/uxb/AbstractDeviceTest.scala
+++ scala/test/edu/cwru/eecs293/ttf10/uxb/AbstractDeviceTest.scala
@@ -4,7 +4,7 @@ import org.junit.Assert._
 import org.junit.{Before, Test}
 
 /**
-  * Class for testing methods in [[edu.cwru.eecs293.ttf10.uxb.AbstractDevice AbstractDevice]] and other underlying functionality thereof. Since the <tt>AbstractDevice</tt> class is obviously abstract, test cases employ concrete subclass [[Hub]] to allow for actualization and access of the superceding functionality in question.
+  * Class for testing methods in [[edu.cwru.eecs293.ttf10.uxb.AbstractDevice AbstractDevice]] and other underlying functionality thereof. Since the <tt>AbstractDevice</tt> class is obviously abstract, test cases employ concrete subclass [[edu.cwru.eecs293.ttf10.uxb.Hub Hub]] to allow for actualization and access of the superceding functionality in question.
   *
   * Case Western Reserve University
   * EECS 293  |  Software Craftsmanship  |  2016 Fall Semester
