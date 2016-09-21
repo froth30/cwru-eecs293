d579c346b4fbd7022d3cd8e274393fc34766f13e
teddybear02 <theo.frohlich@gmail.com>
2016-09-21 02:23:18 -0400

    Inserted paragraph markup elements

--- scala/test/edu/cwru/eecs293/ttf10/uxb/AbstractDeviceTest.scala
+++ scala/test/edu/cwru/eecs293/ttf10/uxb/AbstractDeviceTest.scala
@@ -3,9 +3,9 @@ package edu.cwru.eecs293.ttf10.uxb
 import org.junit.Assert._
 import org.junit.{Before, Test}
 
-/**
+/** <p>
   * Class for testing methods in [[edu.cwru.eecs293.ttf10.uxb.AbstractDevice AbstractDevice]] and other underlying functionality thereof. Since the <tt>AbstractDevice</tt> class is obviously abstract, test cases employ concrete subclass [[edu.cwru.eecs293.ttf10.uxb.Hub Hub]] to allow for actualization and access of the superceding functionality in question.
-  *
+  * <p>
   * Case Western Reserve University
   * EECS 293  |  Software Craftsmanship  |  2016 Fall Semester
   * Programming Assignment 2  |  Due at beginning of discussion on Wednesday, September 7, 2016
