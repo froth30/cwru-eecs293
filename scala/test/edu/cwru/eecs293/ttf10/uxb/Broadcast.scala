223675e67d189e85cfbfa2f9b9c16888d94e33f0
teddybear02 <theo.frohlich@gmail.com>
2016-10-05 07:17:14 -0400

    Made fields protected

--- scala/test/edu/cwru/eecs293/ttf10/uxb/Broadcast.scala
+++ scala/test/edu/cwru/eecs293/ttf10/uxb/Broadcast.scala
@@ -15,8 +15,8 @@ string message.
   */
 class Broadcast {
 
-  private var devices: List[Device] = _
-  private var messages: List[Message] = _
+  protected var devices: List[Device] = _
+  protected var messages: List[Message] = _
 
   @Before
   def setUp() {
