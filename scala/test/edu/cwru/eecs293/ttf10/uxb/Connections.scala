c828155d3a2a7e0a96689f37c93a21dffe647565
teddybear02 <theo.frohlich@gmail.com>
2016-10-05 07:27:49 -0400

    Added class for testing device connections

---
+++ scala/test/edu/cwru/eecs293/ttf10/uxb/Connections.scala
@@ -0,0 +1,23 @@
+package edu.cwru.eecs293.ttf10.uxb
+
+import org.junit.Test
+
+/**
+  * Test class for making sure that the primary methods work as intended.
+  *
+  * @since Programming Assignment 4
+  * <br> <i>
+  * <br> Case Western Reserve University
+  * <br> EECS 293: Software Craftsmanship
+  * <br> 2016 Fall Semester
+  * @author Ted Frohlich < ttf10@case.edu >
+  */
+class Connections extends Broadcast {
+  
+  @Test
+  @throws[Exception]
+  def connect() {
+    
+  }
+  
+}
