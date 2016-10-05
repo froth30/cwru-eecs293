6f09d623cfa71aa86acebe91046b4fbd463c6d99
teddybear02 <theo.frohlich@gmail.com>
2016-10-05 07:05:43 -0400

    Updated header doc

--- scala/test/edu/cwru/eecs293/ttf10/uxb/AbstractDeviceTest.scala
+++ scala/test/edu/cwru/eecs293/ttf10/uxb/AbstractDeviceTest.scala
@@ -6,13 +6,12 @@ import org.junit.{Before, Test}
 /** <p>
   * Class for testing methods in [[edu.cwru.eecs293.ttf10.uxb.AbstractDevice AbstractDevice]] and other underlying functionality thereof. Since the <tt>AbstractDevice</tt> class is obviously abstract, test cases employ concrete subclass [[edu.cwru.eecs293.ttf10.uxb.Hub Hub]] to allow for actualization and access of the superceding functionality in question.
   * <p>
-  * Case Western Reserve University
-  * EECS 293  |  Software Craftsmanship  |  2016 Fall Semester
-  * Programming Assignment 2  |  Due at beginning of discussion on Wednesday, September 7, 2016
-  *
-  * @see [[https://blackboard.case.edu/bbcswebdav/pid-1379538-dt-content-rid-4276971_1/courses/eecs293_vxl11/Hw2.pdf Hw2.pdf]]
-  *
-  * @author Theodore Frohlich <ttf10@case.edu>
+  * @since Programming Assignment 2
+  * <br> <i>
+  * <br> Case Western Reserve University
+  * <br> EECS 293: Software Craftsmanship
+  * <br> 2016 Fall Semester
+  * @author Ted Frohlich < ttf10@case.edu >
   */
 class AbstractDeviceTest {
 
